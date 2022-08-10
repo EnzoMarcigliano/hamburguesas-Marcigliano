e51804135731b26d3086e12ffd4d8cee9fc9ec44
EnzoMarcigliano <enzomarcig96@gmail.com>
2022-08-10 20:57:49 -0300

    cambios

--- src/Component/BurguerButton.jsx
+++ src/Component/BurguerButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useEffect, useState } from "react";
 
 function BurguerButton(props) {
     return (
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import { useEffect, useState } from "react";
+import BurguerButton from './BurguerButton';
 
 function Navbar() {
 
